45f0f6f533af88921e0a94d781806b3b1574ebab
Hamza ZOUBAIR <91201878+HamzaZoubair@users.noreply.github.com>
2023-12-18 21:57:08 +0100

    add new generator with midutil : generate tracks , notes and midi file

--- src/cli/generator.ts
+++ src/cli/generator.ts
@@ -1,4 +1,4 @@
-import type {Music,Note,Track, TrackSet, TrackPart, TimeSignature} from '../language/generated/ast.js';
+import type {Music,Note,Track, TrackSet, TimeSignature} from '../language/generated/ast.js';
 import * as fs from 'node:fs';
 import { CompositeGeneratorNode, toString } from 'langium';
 import * as path from 'node:path';
@@ -6,7 +6,7 @@ import { extractDestinationAndName } from './cli-util.js';
 
 export function generateMusicFile(music: Music,filePath: string, destination: string | undefined): string {
     const data = extractDestinationAndName(filePath, destination);
-    const generatedFilePath = `${path.join(data.destination, data.name)}.js`;
+    const generatedFilePath = `${path.join(data.destination, data.name)}.py`;
 
     const fileNode = new CompositeGeneratorNode();
     compile(music, fileNode)
@@ -29,28 +29,31 @@ function compile(music: Music, fileNode: CompositeGeneratorNode): void {
     for (const track_set of music.trackSet) {
         compileTrackSet(track_set, music.tempo, track_set.time_signature , fileNode);
     }
+    generateMidiFile(fileNode);
+
 
     // TODO : Est ce encore utile ?
-    generateMidiFile(music.tracks, fileNode, music.name);
+    //generateMidiFile(music.tracks, fileNode, music.name);
 }
 
 function compileTrackSet(track_set: TrackSet, tempo: any, time_signature: TimeSignature, fileNode: CompositeGeneratorNode) {
 
     for(let i = 0; i < track_set.track.length; i++){
         fileNode.append(
-            `midi_${i} = MIDIFile(${i})\n`
+            `midi = MIDIFile(${i})\n`
         );
         fileNode.append(
-            `midi_${i}.addTempo(0, 0, ${tempo})\n`
+            `midi.addTempo(0, 0, ${tempo})\n`
         );
         fileNode.append(
             `time_signature = ( ${time_signature.numerator} ,  ${time_signature.denominator} )\n`
         );
         fileNode.append(
-            `midi_${i}.addTimeSignature(0, 0, *time_signature, 24)\n`
+            `midi.addTimeSignature(0, 0, *time_signature, 24)\n`
         );
 
         compileTrack(track_set.track[i], i, fileNode);
+
     }
 
 
@@ -59,15 +62,21 @@ function compileTrackSet(track_set: TrackSet, tempo: any, time_signature: TimeSi
 function compileTrack(track: Track,  track_number:any, fileNode: CompositeGeneratorNode): void {
     for(const track_part of track.parts){
         // TODO : Pourquoi repeat est considere comme un String .. ?
-        for(let i = 0; i < track_part.repeat(); i++){
-            let instrument_number = compileInstrument(track_part, track.instrument.name.toUpperCase(), fileNode)
-            compileNote(track_part.notes)
+        if( track_part.repeat == undefined){
+            //let instrument_number = compileInstrument(track_part, track.instrument.name.toUpperCase(), fileNode)
+            compileNote(track_part.notes,track_number, fileNode)
+        }
+
+        for(let i = 0; i < track_part.repeat!; i++){
+            //let instrument_number = compileInstrument(track_part, track.instrument.name.toUpperCase(), fileNode)
+            compileNote(track_part.notes,track_number, fileNode)
         }
     }
+
 }
 
 
-function compileInstrument(track_part : TrackPart, instrument:String ,fileNode : CompositeGeneratorNode ) {
+/*function compileInstrument(track_part : TrackPart, instrument:String ,fileNode : CompositeGeneratorNode ) {
 
     // TODO : return number of chanel
 
@@ -84,16 +93,28 @@ function compileInstrument(track_part : TrackPart, instrument:String ,fileNode :
         default:
             return ''; // Handle other instrument cases or return an empty string if none matches
     }
-}
+}*/
 
 
-function compileNotes(notes: Note[], i:number, track : Track, fileNode: CompositeGeneratorNode): void {
+function compileNote(notes: Note[], track_number : any, fileNode: CompositeGeneratorNode): void {
+
+    for(const note of notes){
+        fileNode.append(
+            `midi.addNote(${track_number}, 0, "${note.pitch}", ${note.position} ,${note.duration}, ${note.volume})\n`
+        );
+    }
 
     // TODO : mettre les notes
 
 }
 
-function generateMidiFile(tracks:Track[], fileNode:CompositeGeneratorNode, musicName:String) {
+function generateMidiFile(fileNode:CompositeGeneratorNode) {
+    fileNode.append(
+        `with open("output.mid", "wb") as output_file:\n`
+    );
+    fileNode.append(
+        `    midi.writeMIDI(output_file)\n`
+    );
 
 }
 
