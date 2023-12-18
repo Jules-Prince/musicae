45ed30d109616114059d9c5eace897af7e70aebe
Theophile-Yvars <79571637+Theophile-Yvars@users.noreply.github.com>
2023-12-18 20:45:43 +0100

    :rocket:

--- src/cli/generator.ts
+++ src/cli/generator.ts
@@ -26,12 +26,11 @@ function compile(music: Music, fileNode: CompositeGeneratorNode): void {
 
     );
 
-
-
     for (const track_set of music.trackSet) {
         compileTrackSet(track_set, music.tempo, track_set.time_signature , fileNode);
     }
 
+    // TODO : Est ce encore utile ?
     generateMidiFile(music.tracks, fileNode, music.name);
 }
 
@@ -50,45 +49,37 @@ function compileTrackSet(track_set: TrackSet, tempo: any, time_signature: TimeSi
         fileNode.append(
             `midi_${i}.addTimeSignature(0, 0, *time_signature, 24)\n`
         );
+
+        compileTrack(track_set.track[i], i, fileNode);
     }
+
+
 }
 
-function compileTrack(track: Track, tempo:number,  fileNode: CompositeGeneratorNode): void {
-    fileNode.append(
-        `const track${track.id} = new MidiWriter.Track();\n`
-    );
-    const codeOfInstrument : string  = compileInstrument(track, fileNode);
-    fileNode.append(
-        `track${track.id}.addEvent(${codeOfInstrument});\n`
-    );
-    // set tempo
-    fileNode.append(
-        `track${track.id}.setTempo(${tempo});\n`
-    );
-
-    for (const trackPart of track.parts){
-        fileNode.append(
-            `track${track.id}.setTimeSignature(${trackPart.time_sign?.numerator},${trackPart.time_sign?.denominator});\n`
-        );
-        for (const bar of trackPart.bars) {
-            for (let i = 0; i < bar.repeat; i++) {
-                compileBar(bar, i, track, fileNode);
-            }
+function compileTrack(track: Track,  track_number:any, fileNode: CompositeGeneratorNode): void {
+    for(const track_part of track.parts){
+        // TODO : Pourquoi repeat est considere comme un String .. ?
+        for(let i = 0; i < track_part.repeat(); i++){
+            let instrument_number = compileInstrument(track_part, track.instrument.name.toUpperCase(), fileNode)
+            compileNote(track_part.notes)
         }
     }
 }
 
 
-function compileInstrument(track : Track, fileNode : CompositeGeneratorNode ) {
-    switch (track.instrument.name.toUpperCase()) {
+function compileInstrument(track_part : TrackPart, instrument:String ,fileNode : CompositeGeneratorNode ) {
+
+    // TODO : return number of chanel
+
+    switch (instrument) {
         case 'PIANO':
-            return "new MidiWriter.ProgramChangeEvent({ channel: 0 })";
+            return "";
         
         case 'GUITAR':
-            return "new MidiWriter.ProgramChangeEvent({ channel: 6 })"; 
+            return "";
         
         case 'DRUM':
-            return "new MidiWriter.ProgramChangeEvent({ channel: 9 })"; 
+            return "";
         
         default:
             return ''; // Handle other instrument cases or return an empty string if none matches
@@ -96,56 +87,14 @@ function compileInstrument(track : Track, fileNode : CompositeGeneratorNode ) {
 }
 
 
+function compileNotes(notes: Note[], i:number, track : Track, fileNode: CompositeGeneratorNode): void {
 
-function compileBar(bar: Bar, i:number, track : Track, fileNode: CompositeGeneratorNode): void {
-    for (let j = 0; j < bar.beats.length; j++) {
-        compileBeat(bar.beats[j], j, track, fileNode);
-    }
-}
-
-function compileBeat(beat: Beat, i:number, track : Track, fileNode: CompositeGeneratorNode): void {
-    for (let j = 0; j < beat.notes.length; j++) {
-        compileNote(beat.notes[j], j, track, fileNode);
-    }
-}
-
-
-function compileNote(note: Note, i:number, track : Track, fileNode: CompositeGeneratorNode): void {
-    // create an array res to store the notes
-    let res : string[] = [];
-    for (const pitch of note.pitch.values) {
-        // add quotes to every pitch
-        res.push(`'${pitch}'`);
-    }
-    
-
-    fileNode.append(
-        `track${track.id}.addEvent(new MidiWriter.NoteEvent({pitch: [${res}], duration: '${note.duration}'}));\n`
-    );
+    // TODO : mettre les notes
 
 }
 
 function generateMidiFile(tracks:Track[], fileNode:CompositeGeneratorNode, musicName:String) {
-    const trackParams = tracks.map((track) => `track${track.id}`).join(',');
-    
-    fileNode.append(`const writer = new MidiWriter.Writer([${trackParams}]);\n`);
-    fileNode.append(`\n`);
-    fileNode.append(`// Build the MIDI file\n`);
-    fileNode.append(`const builtMidi = writer.buildFile();\n`);
-    fileNode.append(`\n`);
-    fileNode.append(`// Output the MIDI file\n`);
-    fileNode.append(`console.log(builtMidi);\n`);
 
-
-
-    // Spécifiez le chemin du fichier .mid que vous souhaitez créer
-    fileNode.append(`const filePath = \'output/output_${musicName}.mid\';\n`)
-    fileNode.append('const midiData = new Uint8Array(builtMidi);\n')
-    // Écrivez les données dans le fichier
-    fileNode.append('writeFileSync(filePath, Buffer.from(midiData));\n')
-    fileNode.append('console.log(`Le fichier MIDI a été généré avec succès à l\'emplacement : ${filePath}`);\n')
-    // Output the MIDI file
-    fileNode.append('console.log(builtMidi);\n')
 }
 
 
