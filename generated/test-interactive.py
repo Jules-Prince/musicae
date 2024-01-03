1956d096235272bc655961c28b74c13062e0c02f
AMEDOMEY Romeo David <davidromeoamedomey@yahoo.com>
2024-01-03 06:36:03 +0100

    fix: remove midi file creation

--- generated/test-interactive.py
+++ generated/test-interactive.py
@@ -1,5 +1,4 @@
 import pygame.midi
-from midiutil import MIDIFile
 from pynput import keyboard
 import time
 
@@ -9,10 +8,7 @@ midi_output = pygame.midi.Output(0)  # Open the first MIDI port
 
 # MIDI file setup
 tempo = 118  # beats per minute
-midi_file = MIDIFile(1)  # One track
 track = 0
-midi_file.addTrackName(track, 0, "Track")
-midi_file.addTempo(track, 0, tempo)
 
 # Define a mapping from keyboard keys to MIDI notes
 key_to_note = {
@@ -26,6 +22,7 @@ key_to_note = {
 keys_pressed = {}
 start_time = time.time()
 
+
 def on_press(key):
     try:
         if key.char in key_to_note and key.char not in keys_pressed:
@@ -36,26 +33,20 @@ def on_press(key):
     except AttributeError:
         pass
 
+
 def on_release(key):
     try:
         if key.char in key_to_note and key.char in keys_pressed:
             note = key_to_note[key.char]
             midi_output.note_off(note, 100)  # Note off with velocity 100
-            start_note_time = keys_pressed.pop(key.char)
-            note_duration_sec = time.time() - start_note_time  # Duration in seconds
-            # Convert duration from seconds to beats
-            beats_per_second = tempo / 60.0
-            note_duration_beats = note_duration_sec * beats_per_second
-            midi_file.addNote(track, 0, note, start_note_time - start_time, note_duration_beats, 100)
+            keys_pressed.pop(key.char)
     except AttributeError:
         pass
     if key == keyboard.Key.esc:
         # Save the MIDI file and exit program when Esc key is pressed
-        with open("output.mid", "wb") as output_file:
-            midi_file.writeFile(output_file)
-        midi_output.close()
         pygame.midi.quit()
         return False
+
 
 # Collect keyboard events
 listener = keyboard.Listener(
